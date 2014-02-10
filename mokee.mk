78f2d948f1a464112182738629868b1646491ab6
martincz <martincz@mokeedev.com>
2014-02-10 17:05:25 +0800

    mokee bringup
    Change-Id: Ifc47b01db77e6563e6f117f04bdb281827b80fc3

--- mokee.mk
+++ mokee.mk
@@ -1,13 +1,13 @@
 $(call inherit-product, device/samsung/hlte/full_hlte.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/gsm.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += \
@@ -17,4 +17,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="xxxxxxxxxx"
 
 PRODUCT_DEVICE := hlte
-PRODUCT_NAME := cm_hlte
+PRODUCT_NAME := mk_hlte
