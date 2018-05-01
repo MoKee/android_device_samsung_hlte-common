9baf854102321d625a0f50ebe3e2fbe70169eccf
Martincz Gao <martincz@mokeedev.com>
2018-05-01 15:37:53 +0800

    hlte-common: mokee-ify
    Change-Id: I74dc3f2c6844f364d3f88ad0eb4cdfc2e15d613f

--- hlte.mk
+++ hlte.mk
@@ -21,7 +21,7 @@ $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-mokee
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
@@ -57,7 +57,7 @@ PRODUCT_PACKAGES += \
 
 # Gestures
 PRODUCT_PACKAGES += \
-    org.lineageos.keyhandler
+    org.mokee.keyhandler
 
 # Input device
 PRODUCT_COPY_FILES += \
